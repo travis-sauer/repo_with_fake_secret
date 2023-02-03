9f545ff42fa8c8af0ad5b7d2e108111d556b64f7
travis-sauer <109311577+travis-sauer@users.noreply.github.com>
2023-02-03 07:29:33 -0600

    Create fake_credentials.py

---
+++ fake_credentials.py
@@ -0,0 +1,4 @@
+# this file is intended to test gitleaks to detect secrets within a public repo
+
+AWS_SECRET_ACCESS_KEY='12312dfadfsdfsdfds12321321dfadfdasfsdfas'
+AWS_ACCESS_KEY='132132132142dfadsfsdfdsfasdfdafsdfdsfads'
